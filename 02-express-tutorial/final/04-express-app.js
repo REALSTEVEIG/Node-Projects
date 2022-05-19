cf9aa3f973155ab992c659baaf92a6ed73ccae7b
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-05-19 14:53:25 +0100

    Basic to advanced node projects

---
+++ 02-express-tutorial/final/04-express-app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+})
+
+
+/*const express = require('express')
+
+const data = express()
+
+data.use(express.static('./public'))
+
+data.all('*', (req, res) => {
+    res.status(404).send('<h1>Page Doesent Exist!')
+})
+
+data.listen(5000, ()=> {
+    console.log('listening on port 5000...')
+})*/
